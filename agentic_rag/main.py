a0149d00bc44a7b40c47dad6db15b0f139d43caf
abhilashhari1313 <abhilashhari1313>
2025-01-28 10:44:06 +0530

    Agentic Rag

---
+++ agentic_rag/main.py
@@ -0,0 +1,30 @@
+from phi.agent import Agent
+from phi.model.ollama import Ollama
+from phi.embedder.ollama import OllamaEmbedder
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.lancedb import LanceDb, SearchType
+from dotenv import load_dotenv
+
+load_dotenv()
+
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+    vector_db=LanceDb(
+        table_name="recipes",
+        uri="tmp/lancedb",
+        search_type=SearchType.vector,
+        embedder=OllamaEmbedder(),
+    ),
+)
+
+knowledge_base.load()
+
+agent = Agent(
+    model=Ollama(id="llama3.2"),
+    knowledge=knowledge_base,
+    show_tool_calls=True,
+    markdown=True,
+)
+agent.print_response(
+    "How do I make chicken and galangal in coconut milk soup", stream=True
+)
